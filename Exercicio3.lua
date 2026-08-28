65bf7d132497e098a4f2ca25c5c30bde012578e2
Fabrício Jr <fabricioramos396@gmail.com>
2026-08-27 21:28:36 -0300

    Create Exercicio3.lua

---
+++ Exercicio3.lua
@@ -0,0 +1,22 @@
+print("Digite o primeiro valor")
+local N = tonumber(io.read())
+local elementos = {}
+local i
+
+for i = 1, N do
+
+    print("Digite o elemento")
+    local numero = tonumber(io.read())
+    elementos[i] = numero
+end
+
+print("Digite um numero")
+local x = tonumber(io.read())
+
+for i = 1, x do
+    if elementos[i] > x then
+        print("Digite o elemento " .. i .. ": " .. elementos[i])
+    else
+    end
+        
+end
